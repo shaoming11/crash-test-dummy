e9f0d50018efd780b56b22d4ca3c43f60aa8996f
shao <68085490+shaoming11@users.noreply.github.com>
2025-06-06 11:07:37 -0400

    trials

--- src/plot.py
+++ src/plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the data from the CSV file
-df = pd.read_csv('src/data.csv')
+df = pd.read_csv('src/trial3.csv')
 
 # Plot the acceleration-time graph
 plt.figure(figsize=(2000,6))
